26bc99c878c8ffda3b195a1e5ef61f107bf23d1b
Adriram04 <adriramgil@gmail.com>
2024-07-02 13:14:50 +0200

    commit proyectos

---
+++ Descuento2/DeliverUS-Backend/src/models/Product.js
@@ -0,0 +1,40 @@
+import { Model } from 'sequelize'
+const loadModel = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+      const OrderProducts = sequelize.define('OrderProducts', {
+        quantity: DataTypes.INTEGER,
+        unityPrice: DataTypes.DOUBLE
+      })
+
+      Product.belongsTo(models.Restaurant, { foreignKey: 'restaurantId', as: 'restaurant', onDelete: 'cascade' })
+      Product.belongsTo(models.ProductCategory, { foreignKey: 'productCategoryId', as: 'productCategory' })
+      Product.belongsToMany(models.Order, { as: 'orders', through: OrderProducts })
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.DOUBLE,
+    image: DataTypes.STRING,
+    order: DataTypes.INTEGER,
+    basePrice: {
+      allownull: false,
+      type: DataTypes.DOUBLE
+    },
+    availability: DataTypes.BOOLEAN,
+    restaurantId: DataTypes.INTEGER,
+    productCategoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Product'
+  })
+  return Product
+}
+export default loadModel
---
+++ Pinnes/DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -0,0 +1,134 @@
+import { Restaurant, Product, RestaurantCategory, ProductCategory } from '../models/models.js'
+import { Sequelize } from 'sequelize'
+
+const index = async function (req, res) {
+  try {
+    const restaurants = await Restaurant.findAll(
+      {
+        attributes: { exclude: ['userId'] },
+        include:
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      },
+        order: [[{ model: RestaurantCategory, as: 'restaurantCategory' }, 'name', 'ASC']]
+      }
+    )
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const indexOwner = async function (req, res) {
+  try {
+    const restaurants = await Restaurant.findAll({
+      attributes: {
+        exclude: ['userId'],
+        include: [
+          // Añadimos una columna virtual para determinar si el restaurante está fijado
+          [Sequelize.fn('COALESCE', Sequelize.col('pinnedAt'), 0), 'isPinned']
+        ]
+      },
+      where: { userId: req.user.id },
+      include: [{
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [
+        // Primero ordenamos por isPinned descendente (fijados primero)
+        ['isPinned', 'DESC'],
+        // Luego ordenamos por pinnedAt ascendente
+        ['pinnedAt', 'ASC']
+      ]
+    })
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const create = async function (req, res) {
+  const newRestaurant = Restaurant.build(req.body)
+  newRestaurant.userId = req.user.id // usuario actualmente autenticado
+  try {
+    if (newRestaurant.pinned) {
+      newRestaurant.pinnedAt = new Date()
+    }
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const show = async function (req, res) {
+  // Only returns PUBLIC information of restaurants
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+    }
+    )
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const update = async function (req, res) {
+  try {
+    await Restaurant.update(req.body, { where: { id: req.params.restaurantId } })
+    const updatedRestaurant = await Restaurant.findByPk(req.params.restaurantId)
+    res.json(updatedRestaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const destroy = async function (req, res) {
+  try {
+    const result = await Restaurant.destroy({ where: { id: req.params.restaurantId } })
+    let message = ''
+    if (result === 1) {
+      message = 'Sucessfuly deleted restaurant id.' + req.params.restaurantId
+    } else {
+      message = 'Could not delete restaurant.'
+    }
+    res.json(message)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+const pineado = async function (req, res) {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    await Restaurant.update(
+      { pinnedAt: restaurant.pinnedAt ? null : new Date() },
+      { where: { id: restaurant.id } }
+    )
+    const updatedRestaurant = await Restaurant.findByPk(req.params.restaurantId)
+    res.json(updatedRestaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+const RestaurantController = {
+  index,
+  indexOwner,
+  create,
+  show,
+  update,
+  destroy,
+  pineado
+}
+export default RestaurantController
